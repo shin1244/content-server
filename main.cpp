41b3bd123d085a17482b4fe6a30f733dbe48c5c9
신준규 <tlswnsrb1357@gmail.com>
2026-07-22 16:53:08 +0900

    boost/asio 써보기

--- main.cpp
+++ main.cpp
@@ -1,65 +1,56 @@
-#include <winsock2.h>
-#include <mutex>
-#include <thread> 
-#include <iostream>
+#include <boost/asio.hpp>
+#include <memory>
 
-#pragma comment(lib, "ws2_32.lib")
+using boost::asio::ip::tcp;
 
-static HANDLE g_iocp;
+class Session : public std::enable_shared_from_this<Session> {
+public:
+    explicit Session(tcp::socket socket) : socket_(std::move(socket)) {}
 
-int main()
-{
-    WSADATA wsa;
-    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
-        std::cout << "WSAStartup failed\n";
-        return 1;
-    }
-    std::cout << "Winsock ready\n";
+    void start() { read(); }
 
-    g_iocp = CreateIoCompletionPort(INVALID_HANDLE_VALUE, nullptr, 0, 0);
-    if (g_iocp == nullptr) {
-        std::cout << "CreateIoCompletionPort failed: " << GetLastError() << "\n";
-        WSACleanup();
-        return 1;
-    }
-    std::cout << "IOCP created\n";
-
-    const unsigned int n = std::thread::hardware_concurrency() - 1;
-    std::cout << "spawning " << n << " worker threads\n";
-
-    SOCKET listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (listenSocket == INVALID_SOCKET) {
-        std::cout << "socket failed: " << WSAGetLastError() << "\n";
-        CloseHandle(g_iocp);
-        WSACleanup();
-        return 1;
+private:
+    void read() {
+        auto self = shared_from_this();
+        socket_.async_read_some(boost::asio::buffer(buf_),
+            [this, self](boost::system::error_code ec, std::size_t n) {
+                if (!ec) write(n);   // 에코
+            });
     }
 
-    sockaddr_in addr = {};
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    addr.sin_port = htons(5050);
-
-    if (bind(listenSocket, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)) == SOCKET_ERROR) {
-        std::cout << "bind failed: " << WSAGetLastError() << "\n";
-        closesocket(listenSocket);
-        CloseHandle(g_iocp);
-        WSACleanup();
-        return 1;
+    void write(std::size_t n) {
+        auto self = shared_from_this();
+        boost::asio::async_write(socket_, boost::asio::buffer(buf_, n),
+            [this, self](boost::system::error_code ec, std::size_t) {
+                if (!ec) read();
+            });
     }
 
-    if (listen(listenSocket, SOMAXCONN) == SOCKET_ERROR) {
-        std::cout << "listen failed: " << WSAGetLastError() << "\n";
-        closesocket(listenSocket);
-        CloseHandle(g_iocp);
-        WSACleanup();
-        return 1;
+    tcp::socket socket_;
+    std::array<char, 1024> buf_;
+};
+
+class Server {
+public:
+    Server(boost::asio::io_context& io, unsigned short port)
+        : acceptor_(io, tcp::endpoint(tcp::v4(), port)) {
+        accept();
     }
-    std::cout << "listening on port 5050...\n";
 
-    closesocket(listenSocket);
-    CloseHandle(g_iocp);
-    WSACleanup();
+private:
+    void accept() {
+        acceptor_.async_accept(
+            [this](boost::system::error_code ec, tcp::socket socket) {
+                if (!ec) std::make_shared<Session>(std::move(socket))->start();
+                accept();
+            });
+    }
 
-    return 0;
+    tcp::acceptor acceptor_;
+};
+
+int main() {
+    boost::asio::io_context io;
+    Server server(io, 8080);
+    io.run();   // 이벤트 루프
 }
